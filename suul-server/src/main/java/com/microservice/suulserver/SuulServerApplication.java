d9b04f8233d3ac9451478256dd57c5f071bca95b
lizihao <lizihaojlex@gmail.com>
2019-02-17 21:07:20 +0800

    init

---
+++ suul-server/src/main/java/com/microservice/suulserver/SuulServerApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.suulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuulServerApplication.class, args);
+    }
+
+}
